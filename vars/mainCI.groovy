6bb17c643a9b96879da4599951996af160ef9972
shuja <shuja.hafez@gmail.com>
2024-08-26 15:29:03 +0530

    code checkout and ecr build and push

--- vars/mainCI.groovy
+++ vars/mainCI.groovy
@@ -1,11 +1,27 @@
 def call(){
     node('ci-server'){
-        if(env.TAG_NAME ==~ ".*"){
+
+           stage('CodeCheckout') {
+               sh "find ."
+               sh "find . | sed -e '1d' |xargs rm -rf"
+               if(env.TAG_NAME ==~ ".*") {
+                  env.branch_name = "refs/tags/${env.TAG_NAME}"
+               } else {
+                 env.branch_name = "${env.BRANCH_NAME}"
+               }
+               checkout scmGit(
+                       branches: [[name: "${branch_name}"]],
+                       userRemoteConfigs: [[url: "https://github.com/s-devops-i1/expense-${component}"]]
+                    )
+                }
+
+           if(env.TAG_NAME ==~ ".*"){
             stage('Build Code'){
-                print 'OK'
+                sh 'docker build -t 471112569439.dkr.ecr.us-east-1.amazonaws.com/expense-${component}:${TAG_NAME} .'
             }
             stage('Release Software'){
-                print 'OK'
+               sh  'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 471112569439.dkr.ecr.us-east-1.amazonaws.com'
+               sh  'docker push 471112569439.dkr.ecr.us-east-1.amazonaws.com/expense-${component}:${TAG_NAME}'
             }
         }else {
             stage('Lint code'){
